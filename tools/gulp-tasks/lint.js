082d13b8657a3716671e9fea3b2e9df37348163c
liqingwei <liqingwei@shimo.im>
2019-02-18 14:39:51 +0800

    refactor: remove gulp

--- tools/gulp-tasks/lint.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  eslint: (gulp) => () => {
-    const $ = require('gulp-load-plugins')()
-    return gulp.src(['./lib/', './bin/docsify'])
-      .pipe($.eslint())
-      .pipe($.eslint.format())
-      .pipe($.eslint.failAfterError())
-  }
-}
