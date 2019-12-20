e5dd770960d5bfc82d828457e30177f6f23307a0
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-12-21 00:38:12 +0530

    test: write test for default behaviour

---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+const execa = require('execa');
+const test = require('ava');
+
+const rootCommand = path.join(process.cwd(), 'bin/docsify');
+
+test('shows up help message without any args', async t => {
+  const { stdout } = await execa(rootCommand, {reject: false});
+  t.snapshot(stdout);
+});
