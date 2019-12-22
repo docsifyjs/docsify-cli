518b3efc897409fbb65bb4dbe2dcab0342eba6ff
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-12-22 23:54:04 +0530

    fix: lint codebase

--- e2e/index.js
+++ e2e/index.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
-const path = require('path');
-const execa = require('execa');
-const test = require('ava');
+const path = require('path')
+const execa = require('execa')
+const test = require('ava')
 
-const rootCommand = path.join(process.cwd(), 'bin/docsify');
+const rootCommand = path.join(process.cwd(), 'bin/docsify')
 
 test('shows up help message without any args', async t => {
-    const { stderr } = await execa(rootCommand, {reject: false});
-    t.snapshot(stderr);
-});
+  const {stderr} = await execa(rootCommand, {reject: false})
+  t.snapshot(stderr)
+})
 
 const matchSnapshot = async (t, arg) => {
-  const { stdout } = await execa(rootCommand, [arg]);
-  t.snapshot(stdout);
-};
+  const {stdout} = await execa(rootCommand, [arg])
+  t.snapshot(stdout)
+}
 
-test('shows help with -h flag', matchSnapshot, '-h');
-test('shows help with --help flag', matchSnapshot, '--help');
-test('shows version information with -v flag', matchSnapshot, '-v');
-test('shows version information with --version flag', matchSnapshot, '--version');
+test('shows help with -h flag', matchSnapshot, '-h')
+test('shows help with --help flag', matchSnapshot, '--help')
+test('shows version information with -v flag', matchSnapshot, '-v')
+test('shows version information with --version flag', matchSnapshot, '--version')
