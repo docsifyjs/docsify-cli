f522d07d131541e700078663ebcbf5dc5dce1b9f
Tal Hayut <talhayut.dev@gmail.com>
2020-03-13 10:39:07 +0200

    chore: adding tests

--- e2e/index.js
+++ e2e/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const execa = require('execa')
 const test = require('ava')
 
-const rootCommand = path.join(process.cwd(), 'bin/docsify')
+let rootCommand = path.join(process.cwd(), 'bin/docsify')
 
 test('shows up help message without any args', async t => {
   const {stderr} = await execa(rootCommand, {reject: false})
@@ -12,12 +12,19 @@ test('shows up help message without any args', async t => {
 })
 
 const matchSnapshot = async (t, arg) => {
-  const {stdout} = await execa(rootCommand, [arg])
+  let args = [arg]
+
+  if ((typeof arg) === 'object') {
+    args = [arg[0], ...arg.slice(1)]
+  }
+
+  const {stdout} = await execa(rootCommand, args)
   t.snapshot(stdout)
 }
 
 test('shows help with -h flag', matchSnapshot, '-h')
 test('shows help with --help flag', matchSnapshot, '--help')
+test('shows help with init --help flag', matchSnapshot, ['init', '--help'])
 test('shows version information with -v flag', matchSnapshot, '-v')
 test('shows version information with --version flag', matchSnapshot, '--version')
 
