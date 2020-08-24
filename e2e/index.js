dc014b4945b04c28888e351ac117825fe9d57962
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-08-24 17:45:56 +0530

    test: add helpers

--- e2e/index.js
+++ e2e/index.js
@@ -1,18 +1,16 @@
 'use strict'
 
-const path = require('path')
-const execa = require('execa')
 const test = require('ava')
 
-const rootCommand = path.join(process.cwd(), 'bin/docsify')
+const {run} = require('./helpers/test-utils.js')
 
-test('shows up help message without any args', async t => {
-  const {stderr} = await execa(rootCommand, {reject: false})
+test('shows up help message without any args', t => {
+  const {stderr} = run([], {reject: false})
   t.snapshot(stderr)
 })
 
-const matchSnapshot = async (t, arg) => {
-  const {stdout} = await execa(rootCommand, [arg])
+const matchSnapshot = (t, arg) => {
+  const {stdout} = run([arg])
   t.snapshot(stdout)
 }
 
@@ -21,18 +19,7 @@ test('shows help with --help flag', matchSnapshot, '--help')
 test('shows version information with -v flag', matchSnapshot, '-v')
 test('shows version information with --version flag', matchSnapshot, '--version')
 
-test('rejects promise due to error on passing in an unknown command', async t => {
-  const {stderr} = await execa(rootCommand, ['junkcmd'], {reject: false})
+test('rejects promise due to error on passing in an unknown command', t => {
+  const {stderr} = run(['junkcmd'], {reject: false})
   t.snapshot(stderr)
-})
-
-test('init the docs directory', async t => {
-  // If you get `./test_docs already exists.`, delete the test_docs directory manually.
-  const {stdout} = await execa(rootCommand, ['init', './test_docs'], {reject: false, timeout: 3000})
-  t.snapshot(stdout)
-})
-
-test('init the docs directory twice', async t => {
-  const {stdout} = await execa(rootCommand, ['init', './'], {reject: false, input: 'n'})
-  t.snapshot(stdout)
 })
